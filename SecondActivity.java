e8ef7d1076d6e7291891c3c58aee949a60ad1132
Aditya Kumar <k.aditya00@gmail.com>
2020-01-17 15:54:04 +0530

    [lab1] Calculator App

---
+++ SecondActivity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent intent = getIntent();
+        long ans = intent.getLongExtra("RESULT", 0);
+        TextView result = findViewById(R.id.result);
+        result.setText(Long.toString(ans));
+    }
+}
